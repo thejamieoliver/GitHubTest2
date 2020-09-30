a5a020438b9f3b638f0d6e3a39d929c71a984dab
JAMIE OLIVER <220015262@student.uj.ac.za>
2020-09-30 11:22:08 +0200

    added loops

--- GitHubTest2/Form1.vb
+++ GitHubTest2/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i As Integer = 1 To 5
 
+        Next i
+        Dim x As Integer = 1
+        If x < 4 Then
+
+        End If
+    End Sub
 End Class
